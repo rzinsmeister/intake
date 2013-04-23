7b9663c3e6e13b512d8baf228cce6bffd3bc6aa5
rzinsmeister <ruzioe@gmail.com>
2013-04-23 12:14:31 -0500

    log in added to the report controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,12 @@
 class ReportsController < ApplicationController
 
+  before_filter :check_authentication
+  def check_authentication
+    if not session[:user_id]
+      redirect_to new_session_url
+    end
+  end
+
 
   # GET /reports
   # GET /reports.json
